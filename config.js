314cb7ba4dec81eaa44ce8dff197a7747986dc81
Mike Majer <majer.michal@outlook.com>
2019-06-07 15:31:03 +1000

    admin login, admin news

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: "",
+  keySession: ['YOURKEY'],
+  maxAge: 24 * 60 * 60 * 1000
+}
---
+++ models/news.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const newsSchema = new Schema({
+  title: { type: String, required: [true, 'Title is required'] },
+  description: { type: String, required: [true, 'Description is required'] },
+  created: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('News', newsSchema);

3887a5dce98d382307edfdad7bc3cad72d89488d
Mike Majer <majer.michal@outlook.com>
2019-06-08 18:54:18 +1000

    config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: "",
+  db: "mongodb+srv://Mike:nodeTest@cluster0-5hg7k.mongodb.net/test?retryWrites=true&w=majority",
   keySession: ['YOURKEY'],
   maxAge: 24 * 60 * 60 * 1000
 }
